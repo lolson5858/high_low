0fbd32ddc7304c54fd8493286a3907284477070c
lolson5858 <109360413+lolson5858@users.noreply.github.com>
2022-10-07 12:16:33 -0500

    add error msgs for out of range and NaN

--- script.js
+++ script.js
@@ -31,21 +31,38 @@ function defineNum(number) {
   return num;
 }
 
+function clearResponse () {
+  let errMsg = document.getElementById('errMsg');
+  let msg = document.getElementById('msg');
+  errMsg.classList.add('hide');
+  msg.classList.add('hide');
+}
 
-//function that checks the user's guess for 
+//function that checks the user's guess for validity and proximity to target
 function guessCheck(num) {
   let guess = Number(document.getElementById('guess').value);
-
+  let errMsg = document.getElementById('errMsg');
   let msg = document.getElementById('msg');
-  
 
-  if (guess == num) {
+
+  if (isNaN(guess)) {
+    errMsg.innerHTML = 'That is not a number!';
+    errMsg.style.color = 'red';
+  // } else if (guess > num || guess < 0) {
+  //   errMsg.innerHTML = 'That number is not in range. Please try again!';
+  //   errMsg.style.color = 'red';
+  } else if( guess > Mnumber || guess < 0) {
+    errMsg.innerHTML = 'That number is not in range, please try again.'
+    errMsg.style.color = 'red';
+  } else if (guess == num) {
     msg.innerHTML = "Nice, you got it!!!"
   } else if (guess < num) {
     msg.innerHTML = "It looks like your guess is a bit low. Guess again!"
   } else {
     msg.innerHTML = "Looks like your guess is a little high. Try again!"
+    console.log(guess);
   }
+  errMsg.classList.remove('hide');
   msg.classList.remove('hide');
 }
 
